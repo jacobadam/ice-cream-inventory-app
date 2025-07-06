9204b1f3079b644320c08c2372164f415c4dd7f8
jacobadam <jacobanevitt@gmail.com>
2025-07-06 11:49:24 -0600

    add dashboard and products components, set up routing and test

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./products/product-list/product-list.component').then(
+        (m) => m.ProductListComponent
+      ),
+  },
+];
