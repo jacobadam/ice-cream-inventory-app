9bd647d186d22a292a812dc874d2cb4eb0abc628
jacobadam <jacobanevitt@gmail.com>
2025-07-06 12:21:08 -0600

    import product model and service and display all products

--- frontend/src/app/products/product-list/product-list.component.ts
+++ frontend/src/app/products/product-list/product-list.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-list',
+  standalone: true,
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.productService.getAll();
+  }
 }
