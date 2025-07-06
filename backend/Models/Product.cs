67c7d1f3c54074769e762aee55ff98d220f0a8b2
jacobadam <jacobanevitt@gmail.com>
2025-07-06 15:05:04 -0600

    add product model

---
+++ backend/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace backend.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Flavor { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public int Sold { get; set; }
+}
