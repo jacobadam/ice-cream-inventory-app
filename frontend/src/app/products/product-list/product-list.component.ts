aa49ee18f57028dbc9097fc09f051bc6b0f0bfb3
jacobadam <jacobanevitt@gmail.com>
2025-07-06 12:56:22 -0600

    import product modal to product list and test out modal opening and closing

--- frontend/src/app/products/product-list/product-list.component.ts
+++ frontend/src/app/products/product-list/product-list.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { ProductModalComponent } from '../product-modal/product-modal.component';
 import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductModalComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  private productService = inject(ProductService);
 
   ngOnInit(): void {
     this.products = this.productService.getAll();
@@ -22,4 +23,6 @@ export class ProductListComponent implements OnInit {
     this.productService.delete(id);
     this.products = this.productService.getAll();
   }
+
+  showModal = false;
 }
