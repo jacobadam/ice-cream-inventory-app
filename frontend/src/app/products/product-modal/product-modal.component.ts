f9957bd632e4842c77c2be308d4be31baf9049cd
jacobadam <jacobanevitt@gmail.com>
2025-07-06 13:10:34 -0600

    create basic form for adding products, import to modal and test

--- frontend/src/app/products/product-modal/product-modal.component.ts
+++ frontend/src/app/products/product-modal/product-modal.component.ts
@@ -1,9 +1,11 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { ProductFormComponent } from '../product-form/product-form.component';
 
 @Component({
   selector: 'app-product-modal',
-  imports: [],
+  standalone: true,
   templateUrl: './product-modal.component.html',
+  imports: [ProductFormComponent],
   styleUrl: './product-modal.component.css',
 })
 export class ProductModalComponent {
