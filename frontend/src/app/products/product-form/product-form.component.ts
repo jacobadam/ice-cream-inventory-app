e5269d786046e70f9cc01c818c7ef8685e3bc8d4
jacobadam <jacobanevitt@gmail.com>
2025-07-06 12:32:46 -0600

    add product form component, import angular forms and add routes

---
+++ frontend/src/app/products/product-form/product-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './product-form.component.html',
+  styleUrl: './product-form.component.css',
+})
+export class ProductFormComponent {
+  private fb = inject(FormBuilder);
+}
