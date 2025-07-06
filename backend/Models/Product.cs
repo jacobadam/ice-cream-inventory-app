cb3e3c20ce56d035dc933dc71eb1630b86f93a20
jacobadam <jacobanevitt@gmail.com>
2025-07-06 17:02:16 -0600

    import DataAnnotations and add validation attributes to Product model

--- backend/Models/Product.cs
+++ backend/Models/Product.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.Models;
 
 public class Product
 {
     public int Id { get; set; }
+
+    [Required]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
     public string Flavor { get; set; } = string.Empty;
+
+    [Range(0.0, double.MaxValue)]
     public decimal Price { get; set; }
+
+    [Range(0, int.MaxValue)]
     public int Stock { get; set; }
+
+    [Range(0, int.MaxValue)]
     public int Sold { get; set; }
 }
