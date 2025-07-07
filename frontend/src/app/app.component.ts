afd5adcec816396160c7f5bea1d949827d539764
jacobadam <jacobanevitt@gmail.com>
2025-07-07 11:36:38 -0600

    add boilerplate template for app, import common module and router modole, add toggleside bar method and add icons

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ice-cream-inventory';
+
+  isOpen = true;
+
+  toggleSidebar(): void {
+    this.isOpen = !this.isOpen;
+  }
 }
