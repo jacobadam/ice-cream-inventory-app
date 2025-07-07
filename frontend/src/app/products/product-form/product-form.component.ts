d12e0a5e075ba5e9cdaa2c6092da0f2d5b5a3611
jacobadam <jacobanevitt@gmail.com>
2025-07-06 18:39:18 -0600

    minor refactor and update onSubmit logic to subscribe to add

--- frontend/src/app/products/product-form/product-form.component.ts
+++ frontend/src/app/products/product-form/product-form.component.ts
@@ -6,18 +6,22 @@ import {
   Validators,
 } from '@angular/forms';
 import { ProductService } from '../product.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-form',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css',
 })
 export class ProductFormComponent {
   private fb = inject(FormBuilder);
+  private productService = inject(ProductService);
+
+  @Output() formSubmit = new EventEmitter<void>();
 
   form: FormGroup = this.fb.group({
-    id: [Date.now()],
     name: ['', Validators.required],
     flavor: ['', Validators.required],
     price: [0, [Validators.required, Validators.min(0)]],
@@ -25,14 +29,25 @@ export class ProductFormComponent {
     sold: [0, [Validators.required, Validators.min(0)]],
   });
 
-  @Output() formSubmit = new EventEmitter<void>();
-
-  private productService = inject(ProductService);
-
   onSubmit(): void {
-    if (this.form.valid) {
-      this.productService.add(this.form.value);
-      this.formSubmit.emit();
+    if (this.form.invalid) {
+      return;
     }
+
+    const newProduct: Product = {
+      id: 0,
+      ...this.form.value,
+    };
+
+    this.productService.add(newProduct).subscribe(() => {
+      this.form.reset({
+        name: '',
+        flavor: '',
+        price: 0,
+        stock: 0,
+        sold: 0,
+      });
+      this.formSubmit.emit();
+    });
   }
 }
