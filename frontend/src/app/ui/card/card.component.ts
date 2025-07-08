bf588433cd6ce5daac91f87fe6e45a18f059120d
jacobadam <jacobanevitt@gmail.com>
2025-07-08 11:13:16 -0600

    update styling for mobile

--- frontend/src/app/ui/card/card.component.ts
+++ frontend/src/app/ui/card/card.component.ts
@@ -8,7 +8,9 @@ import { Component, Input } from '@angular/core';
       class="bg-white border border-gray-200 rounded-lg shadow p-5 h-80 overflow-auto"
     >
       @if (title) {
-      <h2 class="text-sm text-gray-500 uppercase tracking-wide mb-3">
+      <h2
+        class="text-center sm:text-left text-sm text-gray-500 uppercase tracking-wide mb-3"
+      >
         {{ title }}
       </h2>
       }
