ff9a03af458e69ec99025ed1a430373ac8666548
jacobadam <jacobanevitt@gmail.com>
2025-07-08 17:58:22 -0600

    update seed data

--- backend/Data/SeedData.cs
+++ backend/Data/SeedData.cs
@@ -17,10 +17,18 @@ namespace backend.Data
                 return;
 
             context.Products.AddRange(
-                new Product { Name = "Vanilla Bean", Flavor = "Vanilla", Price = 3.50M, Stock = 100, Sold = 25 },
-                new Product { Name = "Chocolate Fudge", Flavor = "Chocolate", Price = 4.00M, Stock = 80, Sold = 40 },
-                new Product { Name = "Strawberry Swirl", Flavor = "Strawberry", Price = 3.75M, Stock = 60, Sold = 30 },
-                new Product { Name = "Mint Chip", Flavor = "Mint", Price = 3.85M, Stock = 50, Sold = 15 }
+                new Product { Name = "Fudge Fiesta", Flavor = "Chocolate Fudge", Price = 5.99M, Stock = 19, Sold = 52 },
+                new Product { Name = "Summer Bliss", Flavor = "Strawberry", Price = 4.89M, Stock = 4, Sold = 43 },
+                new Product { Name = "Pure Essence", Flavor = "Vanilla", Price = 4.79M, Stock = 16, Sold = 32 },
+                new Product { Name = "Mint Fusion", Flavor = "Mint Chocolate", Price = 5.29M, Stock = 3, Sold = 26 },
+                new Product { Name = "Cookie Crumble", Flavor = "Cookie Dough", Price = 4.99M, Stock = 3, Sold = 77 },
+                new Product { Name = "Mint Fusion", Flavor = "Mint Chocolate", Price = 4.79M, Stock = 3, Sold = 26 },
+                new Product { Name = "Cherry Grove", Flavor = "Cherry", Price = 5.29M, Stock = 8, Sold = 22 },
+                new Product { Name = "Salted Pistachio", Flavor = "Pistachio", Price = 5.59M, Stock = 3, Sold = 44 },
+                new Product { Name = "Caramel Velvet", Flavor = "Salted Caramel", Price = 4.69M, Stock = 15, Sold = 52 },
+                new Product { Name = "Coffee Espresso", Flavor = "Coffee", Price = 5.29M, Stock = 3, Sold = 26 },
+                new Product { Name = "Peanut Butter Swirl", Flavor = "Peanut Butter", Price = 5.29M, Stock = 3, Sold = 64 },
+
             );
 
             context.SaveChanges();
