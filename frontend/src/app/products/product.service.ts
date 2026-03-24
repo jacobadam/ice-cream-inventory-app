6a3bd323e19dff3987898983369965c50645ab4f
jacobadam <jacobanevitt@gmail.com>
2026-03-24 14:13:05 -0500

    rename environments file and update import

--- frontend/src/app/products/product.service.ts
+++ frontend/src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environments';
+import { environment } from '../../environments/environment';
 import { Product } from './product.model';
 
 @Injectable({ providedIn: 'root' })
